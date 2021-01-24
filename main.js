c339b21f17cc3db7052dcedf945839701893ddb9
Satoshita <62451999+Satoshita@users.noreply.github.com>
2021-01-24 22:49:20 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -13,4 +13,12 @@
   }
 
 
+
+  const btn = document.getElementById('btn');
+
+  btn.addEventListener('click', () => {
+    const result = ['大吉', '中吉', '小吉', '凶','末吉'];
+    btn.textContent = result[Math.floor(Math.random() * result.length)];
+
+  });
 }
